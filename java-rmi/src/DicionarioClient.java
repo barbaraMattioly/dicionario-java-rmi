64dd32653cc1089166c19dde91bbf4015347423f
barbara.andrade <bmattiolyandrade@gmail.com>
2024-03-06 00:31:12 -0300

    chore(code) Add menu

--- java-rmi/src/DicionarioClient.java
+++ java-rmi/src/DicionarioClient.java
@@ -1,27 +1,22 @@
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.List;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.NotBoundException;
 
 public class DicionarioClient {
-
+	private static Dicionario d;
 	public static void main(String[] args) {
 		String servidor = "rmi://localhost/";
 		String nome = "DicionarioService";
 		try {
-			Dicionario d = (Dicionario) Naming.lookup(servidor + nome);
-			System.out.println("Objeto remoto \'"+ nome + "\' encontrado no servidor.");
+			d = (Dicionario) Naming.lookup(servidor + nome);
+			System.out.println("Objeto remoto \'" + nome + "\' encontrado no servidor.");
 
-			String palavra, significado;
-            System.out.println("Digite a palavra: ");
-            palavra = System.console().readLine();
-            System.out.println("Digite o significado: ");
-            significado = System.console().readLine();
-
-			// System.out.println(d.consultar(palavra));
-			// System.out.println(d.adicionar(palavra, significado));
-			// System.out.println(d.remover(palavra));
-		
+			opcoesMenu();
 
 		} catch (MalformedURLException e) {
 			System.out.println("URL \'" + servidor + nome + "\' mal formatada.");
@@ -34,5 +29,90 @@ public class DicionarioClient {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	private static void exibirMenu() {
+		String nomeArquivo = "./java-rmi/src/arquivos/Menu.txt";
+
+		try {
+			List<String> linhas = Files.readAllLines(Paths.get(nomeArquivo));
+
+			for (String linha : linhas) {
+				System.out.println(linha);
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+		}
+	}
+
+	private static void opcoesMenu() throws RemoteException {
+		int opcao = 0;
+		do {
+			exibirMenu();
+			System.out.print("Selecione a opção: ");
+			try {
+				opcao = Integer.parseInt(System.console().readLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Opção inválida.");
+			}
+
+			limparTela();
+			switch (opcao) {
+				case 1:
+					consultarSignificado();
+					break;
+				case 2:
+					adicionarPalavra();
+					break;
+				case 3:
+					removerPalavra();
+					break;
+				default:
+					break;
+			}
+			pausa();
+			limparTela();
+		} while (opcao != 4);
+	}
+
+	private static void consultarSignificado() throws RemoteException {
+		String palavra;
+
+		System.out.print("Digite a palavra: ");
+		palavra = System.console().readLine();
+
+		System.out.println(d.consultar(palavra));
+	}
+
+	private static void adicionarPalavra() throws RemoteException {
+		String palavra, significado;
+
+		System.out.print("Digite a palavra: ");
+		palavra = System.console().readLine();
+
+		System.out.print("\nDigite o significado: ");
+		significado = System.console().readLine();
+
+		d.adicionar(palavra, significado);
+	}
+
+	private static void removerPalavra() throws RemoteException {
+		String palavra;
+
+		System.out.print("Digite a palavra: ");
+		palavra = System.console().readLine();
+
+		d.remover(palavra);
+	}
+
+	private static void limparTela() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	private static void pausa() {
+		System.out.println("Enter para continuar.");
+		System.console().readLine();
 	}
 }
